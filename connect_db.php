274ac9b1a29a8dd50bacc2a17ac1ac83dc5c788b
juthamatkhamman <juthamat.kh@ku.th>
2023-01-30 12:01:26 +0700

    M

---
+++ connect_db.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "cpe0597";
+$password = "@@##&&Pj240502";
+$db_name = "cpe0597";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db_name);
+
+// Check connection
+if ($conn->connect_error) 
+{
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
